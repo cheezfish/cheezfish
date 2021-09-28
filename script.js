731a7df68bc92f4662ed8c3b501f38b48807415a
cheezfish <imran-aziz@mail.com>
2021-09-28 03:37:29 +0100

    finally sorted out my collapsible, connected up contact form
    MVP achieved yay

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ $(document).ready(function(){
     });
   });
 
+//and a thing to make the contact form fuck off
+
+var button = document.getElementById('contact'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('contact-form');
+    if (div.style.display !== 'block') {
+        div.style.display = 'block';
+    }
+    else {
+        div.style.display = 'none';
+    }
+};
