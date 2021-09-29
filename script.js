604fff2bc82b092c2de3978bc827e9e776ea1e3b
cheezfish <imran-aziz@mail.com>
2021-09-29 04:33:31 +0100

    added granim, fixed wonkiness,

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ button.onclick = function() {
         div.style.display = 'none';
     }
 };
+
+var granimInstance = new Granim({
+    element: '#canvas-image-blending',
+    direction: 'top-bottom',
+    isPausedWhenNotInView: true,
+    image : {
+        source: '/profile/squareprofile2.png',
+        blendingMode: 'color-burn'
+    },
+    states : {
+        "default-state": {
+            gradients: [
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#80d3fe', '#7ea0c4'],
+                ['#f0ab51', '#eceba3']
+            ],
+            transitionSpeed: 7000
+        }
+    }
+});
