006439ccc3716a33eda6503b03455063a4f3bafa
cheezfish <imran-aziz@mail.com>
2021-09-29 05:22:56 +0100

    now using cdn for granim

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ button.onclick = function() {
         div.style.display = 'none';
     }
 };
+
+
+//my function lol
+
+
+var granimInstance = new Granim({
+    element: '#canvas-image-blending',
+    direction: 'top-bottom',
+    isPausedWhenNotInView: true,
+    image : {
+        source: '/profile/squareprofile2.png',
+        blendingMode: 'soft-light'
+    },
+    states : {
+        "default-state": {
+            gradients: [
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#80d3fe', '#7ea0c4'],
+                ['#f0ab51', '#eceba3']
+            ],
+            transitionSpeed: 7000
+        }
+    }
+});
